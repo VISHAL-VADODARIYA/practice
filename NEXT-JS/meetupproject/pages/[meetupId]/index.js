f915997e6facc45947aef165f079aa80e051bb73
vishal-vadodariya <vadodariyavishal2@gmail.com>
2023-05-31 14:16:56 +0530

    change

---
+++ NEXT-JS/meetupproject/pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+export default function MeetupDetails(id) {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="A First Meetup"
+      address="Some address 10, 12345 Some City"
+      description="This is a first meetup!"
+    />
+  );
+}
